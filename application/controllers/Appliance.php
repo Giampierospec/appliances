ddc5ecf8196efecfcee0193a496ae1d0b9657b85
Giampiero Specogna <giampi_12@hotmail.com>
2017-03-19 12:52:35 -0400

    added some validation to the app
    modified the templates and added new views

--- application/controllers/Appliance.php
+++ application/controllers/Appliance.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Appliance extends CI_Controller{
@@ -7,11 +8,30 @@ class Appliance extends CI_Controller{
   {
     parent::__construct();
     //Codeigniter : Write Less Do More
+    $currentMethod = $this->router->fetch_method();
+    if(!isset($_SESSION['app_user']) && $currentMethod != 'login'){
+      redirect('appliance/login');
+    }
   }
 
   function index()
   {
+    /*First line loads the top template
+      Second line loads the view you want to see A.K.A the content
+      Third Line loads the footer
+    */
+    $this->load->view('templates/top');
     $this->load->view('admin/start');
+    $this->load->view('templates/bottom');
+  }
+  function login(){
+    /*First line loads the top template
+      Second line loads the view you want to see A.K.A the content
+      Third Line loads the footer
+    */
+    $this->load->view('templates/top');
+    $this->load->view('admin/login');
+    $this->load->view('templates/bottom');
   }
 
 }
---
+++ application/views/templates/bottom.php
@@ -0,0 +1,6 @@
+<footer class="navbar-fixed-bottom bg-orange">
+  <h2>Appliances App by Giampiero Specogna &copy; <?php echo date('Y') ?></h2>
+</footer>
+</div>
+</body>
+</html>
