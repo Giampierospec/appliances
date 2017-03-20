ca6d43536ed6eba6d4256b260f335bf0274b6607
Giampiero Specogna <giampi_12@hotmail.com>
2017-03-19 21:18:51 -0400

    added the insert feature on products table

--- application/views/admin/start.php
+++ application/views/admin/start.php
@@ -1,4 +1,31 @@
-<?php $currentUser = $_SESSION["app_user"];?>
+<?php
+$currentUser = $_SESSION["app_user"];
+$CI =& get_instance();
+$message = "";
+if($_POST){
+  $product = new stdClass();
+  $product->brand = $_POST['brand'];
+  $product->date = $_POST['date'];
+  $product->type = $_POST['type'];
+  $product->weight = $_POST['weight'];
+  $product->color = $_POST['color'];
+  $product->comment = $_POST['comment'];
+  $photo = $_FILES['photos'];
+  $product->imgPath = "/xampp/htdocs/appliances/appliancePhotos/";
+  $product->imgContent = $photo['name'];
+  $product->idUser = $currentUser->id;
+  if($photo['error'] == 0 && ($photo['type'] == 'image/jpeg' || $photo['type'] == 'image/png')){
+    $CI->db->insert('product',$product);
+    move_uploaded_file($photo["tmp_name"],"$product->imgPath"."$product->imgContent");
+  }
+  else{
+    $message = "Eso no es una foto valida vuelva a intentarlo";
+  }
+
+
+}
+
+?>
 
 <div class="text-right">
   <p>Usted ha ingresado como <?php echo $currentUser->email ?> <a href="<?php echo(base_url('appliance/logout')) ?>">|Salir|</a></p>
@@ -9,7 +36,7 @@
       <h2>Ingrese la información de los electrodomesticos</h2>
       <div class="row">
         <div class="col-sm-12">
-          <form class="form-horizontal" action="" method="post">
+          <form enctype="multipart/form-data" class="form-horizontal" action="" method="post">
               <div class="form-group input-group">
                 <label for="brand" class="input-group-addon bg-orange"><i class="fa fa-globe" aria-hidden="true"></i> Marca</label>
                 <input type="text" name="brand" class="form-control" required/>
@@ -24,27 +51,47 @@
               </div>
               <div class="form-group input-group">
                 <label for="weight" class="input-group-addon bg-orange"><i class="fa fa-truck" aria-hidden="true"></i> Peso</label>
-                <input type="numeric" name="weight" class="form-control" required/>
+                <input type="number" step="0.01" name="weight" class="form-control" required/>
               </div>
               <div class="form-group input-group">
                 <label for="color" class="input-group-addon bg-orange"><i class="fa fa-barcode" aria-hidden="true"></i> Color</label>
-                <input type="text" name="color" class="form-control" required/>
+                <input type="color" name="color" class="form-control" required/>
               </div>
               <div class="form-group input-group">
-                <label for="photo" class="input-group-addon bg-orange"><i class="fa fa-camera" aria-hidden="true"></i> Suba su imagen aqui</label>
-                <input type="file" name="photo" class="form-control" accept="image/*" id="photo" required/>
+                <label for="photos" class="input-group-addon bg-orange"><i class="fa fa-camera" aria-hidden="true"></i> Suba su imagen aqui</label>
+                <input type="file" name="photos" class="form-control" accept="image/*" id="photos" required/>
               </div>
               <div class="form-group input-group">
                 <label for="comment" class="input-group-addon bg-orange"><i class="fa fa-comment" aria-hidden="true"></i> Comentario</label>
                 <textarea name="comment" rows="2" cols="80" class="form-control" required></textarea>
               </div>
               <div class="text-center">
-                <button type="submit" id="btn_submit" class="btn bg-orange"><i class="fa fa-floppy-o" aria-hidden="true"></i> Guardar</button>
+                <button type="submit" id="btn_submit_1" class="btn bg-orange"><i class="fa fa-floppy-o" aria-hidden="true"></i> Guardar</button>
               </div>
           </form>
         </div>
       </div>
     </div>
+    <div id="message"class="alert alert-danger" style="display:none;">
+      <p><?php echo $message ?></p>
+    </div>
+    <!-- In this script i will show the message -->
+    <script type="text/javascript">
+      $(document).ready(function() {
+        var message = "<?php echo (isset($message)?$message:"") ?>";
+        if(message != ""){
+          $("#message").fadeIn(5000,appendClose).addClass("alert-dismissable fade in");
+        }
+        else{
+          $("#message").hide();
+        }
+      });
+      function appendClose(){
+        var close = '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>';
+        $(close).appendTo("#message").fadeIn(5000);
+        $()
+      }
+    </script>
   </div>
   <div class="col-sm-6">
     <img src="http://signalysis.com/images/kitchen/kitchen.png" class="img-responsive"alt="appliances">
@@ -64,6 +111,6 @@
     </tr>
   </thead>
   <tbody>
-    
+
   </tbody>
 </table>
